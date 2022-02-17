fef13effb807855122985d759b00a7123ba6da84
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2022-02-17 13:45:21 +0100

    edit ex16: add end point

--- src/ex16-express_template_react/index16.js
+++ src/ex16-express_template_react/index16.js
@@ -1,4 +1,8 @@
 
+const months = ["january", "february", "march", "april",
+    "may", "june", "july", "august",
+    "september", "october","november","december"];
+
 const express = require('express');
 const app = new express();
 const expressReactViews = require('express-react-views');
@@ -13,6 +17,15 @@ app.engine('jsx',jsxEngine);
 
 app.get("/:name",(req,res)=>{
     res.render('index', { name: req.params.name });
+});
+
+app.get("/fetch/:num", (req, res)=>{
+    let num = parseInt(req.params.num);
+    if (num < 1 || num > 12) {
+        res.render('index', {name: "user, next time insert a valid number between 1 and 12!"});
+    } else {
+        res.render('index', {name: months[num-1]});
+    }
 });
 
 app.listen(3333, () => {
