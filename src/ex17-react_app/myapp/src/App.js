72c389b1088d6aaf296bc0fe32c9f1a007f88384
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2022-02-18 16:46:40 +0100

    solving ex17: edit App.js

--- src/ex17-react_app/myapp/src/App.js
+++ src/ex17-react_app/myapp/src/App.js
@@ -1,3 +1,30 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const colorStyle = {
+            color:this.props.color,
+            fontSize:this.props.size+"px"
+        }
+        return (
+            <div>
+                <div style={colorStyle}>
+                    Hello World!
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+/*
 function App(props) {
     const colorStyle = {
         color:props.color,
@@ -13,3 +40,4 @@ function App(props) {
 }
 
 export default App;
+ */
