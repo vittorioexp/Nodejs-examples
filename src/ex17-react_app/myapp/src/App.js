fc061e271d773b5b75b6cb2c29a99821c92192cb
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2022-02-18 16:50:44 +0100

    solving ex17: edit App.js

--- src/ex17-react_app/myapp/src/App.js
+++ src/ex17-react_app/myapp/src/App.js
@@ -5,6 +5,37 @@ class App extends React.Component {
     constructor(props) {
         super(props);
     }
+    state = { counter:0 }
+
+    incrementCounter = ()=> {
+        this.setState({counter:this.state.counter+1});
+    }
+
+    render() {
+        const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
+        return (
+            <div style={colorStyle}>
+                React Component
+                <br /><br />
+                <button onClick={this.incrementCounter}>Click Me!</button>
+                <br /><br />
+                {this.state.counter}
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+/*
+import React from 'react';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         const colorStyle = {
@@ -22,6 +53,9 @@ class App extends React.Component {
 }
 
 export default App;
+ */
+
+
 
 
 /*
