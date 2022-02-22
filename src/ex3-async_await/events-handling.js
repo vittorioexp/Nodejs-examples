057c03134ce39e54e491bcadca2e838d294b1882
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2022-02-22 16:29:26 +0100

    edit ex3: add event handling

---
+++ src/ex3-async_await/events-handling.js
@@ -0,0 +1,15 @@
+
+const EventEmitter = require('events')
+
+const customEmitter = new EventEmitter()
+
+
+customEmitter.on('response', (name, id) => {
+    console.log(`data recieved user ${name} with id:${id}`)
+})
+
+customEmitter.on('response', () => {
+    console.log('some other logic here')
+})
+
+customEmitter.emit('response', 'john', 34)
